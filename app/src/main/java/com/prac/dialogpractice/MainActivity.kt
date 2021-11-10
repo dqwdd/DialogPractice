32bef6f8a3b0692cb3f688b5b5fe83dcb97016c7
dqwdd <dqwdd@naver.com>
2021-11-10 10:47:42 +0900

    확인 버튼 누를 때 이벤트 되게 했는데 이번엔 dateTxt.text가 안됨..

--- app/src/main/java/com/prac/dialogpractice/MainActivity.kt
+++ app/src/main/java/com/prac/dialogpractice/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.prac.dialogpractice
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
+import android.widget.TimePicker
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import com.prac.dialogpractice.databinding.ActivityMainBinding
+import kotlinx.coroutines.NonCancellable.cancel
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +24,19 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    val mSelectedDateTime = Calendar.getInstance()//기본값 = 현재 시간
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        //타임 피커
         binding.showTimePickerDialog.setOnClickListener {
             showTimePickerDialog(View(this))
         }
 
+        //날짜 피커
         binding.showDatePickerDialog.setOnClickListener {
             showDatePickerDialog(View(this))
         }
@@ -29,11 +45,73 @@ class MainActivity : AppCompatActivity() {
 
         datePicker = DatePickerHelper(this, true)
 
+        //스피너 형태의 날짜 피커
         binding.showDatePickerDialog2.setOnClickListener {
             showDatePickerDialog()
         }
 
 
+        //날짜 선택 후 확인 누르면 시간 나오게 하기
+        binding.dateTxt.setOnClickListener {
+
+//            DatePicker 띄우기 -> 입력 완료되면, 연/월/일을 제공해줌
+//            mSelectedDateTime에 연/월/일 저장
+            val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+
+//                    선택된 날짜로서 지정
+                    mSelectedDateTime.set(year, month, day)
+
+//                    선택된 날짜로 문구 변경=> 2021-9-13 (월요일)
+                    val sdf = SimpleDateFormat("yyyy. M. d (E)")
+                    binding.dateTxt.text = sdf.format( mSelectedDateTime.time )
+
+
+                }
+            }
+
+            val dpd = DatePickerDialog(this, dateSetListener,
+                mSelectedDateTime.get(Calendar.YEAR),
+                mSelectedDateTime.get(Calendar.MONTH),
+                mSelectedDateTime.get(Calendar.DAY_OF_MONTH))
+
+            dpd.setButton(DialogInterface.BUTTON_POSITIVE, "확인", DialogInterface.OnClickListener {
+                    dialogInterface, i ->
+                binding.textView.text = "성공"
+            })
+            dpd.show()
+
+            // binding.textView.text = "성공"
+
+        }
+
+
+//        시간 선택
+        binding.timeTxt.setOnClickListener {
+
+//            TimePicker띄우기 -> 입력 완료되면 시/분 제공
+//            mSelectedTime에 시/분 저장
+
+            val tsl = object  : TimePickerDialog.OnTimeSetListener {
+                override fun onTimeSet(p0: TimePicker?, hour: Int, minute: Int) {
+
+                    mSelectedDateTime.set(Calendar.HOUR_OF_DAY, hour)
+                    mSelectedDateTime.set(Calendar.MINUTE, minute)
+
+//                    오후 6:05  형태로 가공. => SimpleDateFormat
+                    val sdf = SimpleDateFormat("a h:mm")
+                    binding.timeTxt.text = sdf.format(mSelectedDateTime.time)
+
+                }
+
+            }
+
+            TimePickerDialog(this, tsl,
+                mSelectedDateTime.get(Calendar.HOUR_OF_DAY),
+                mSelectedDateTime.get(Calendar.MINUTE),
+                false).show()
+
+        }
     }
 
     fun showTimePickerDialog(v: View) {
@@ -61,5 +139,6 @@ class MainActivity : AppCompatActivity() {
                 binding.textView.text = "${dayStr}-${monthStr}-${year}"
             }
         })
+
     }
 }
